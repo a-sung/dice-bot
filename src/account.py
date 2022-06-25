0ee69afc66fb64220165808faee89c8c4bdac2b2
Asung <a.city521@gmail.com>
2022-06-25 17:02:53 +0900

    feat: 봇에게 보내진 메시지 확인

--- src/account.py
+++ src/account.py
@@ -1,1 +1,22 @@
+import tweepy
+
+class Mention:
+    def __init__(self, api, bot):
+        self.last_reply_id = 1
+        self.api = api
+        self.bot = bot
+        self.mentions = None
+        self.receiver = None
+
+    def get_mentions(self):
+        # get new mentions after last_reply_id
+        self.mentions = self.api.mentions_timeline(since_id=self.last_reply_id)
+        self.mentions.reverse()
+
+    def check_mentions(self):
+        if self.mentions is None:
+            print("no new mentions")
+            return
+        for mention in self.mentions:
+            print(mention)
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from auth import TwitterAuth, TwitterBot
+from account import Mention
+import time
 
 def main():
     api = TwitterAuth().get_auth()
     bot = TwitterBot(api).get_bot()
-    api.update_status("dice bot test!!!")
+
+    m = Mention(api, bot)
+    while True:
+        m.get_mentions()
+        m.check_mentions()
+        time.sleep(20)
 
 if __name__ == "__main__":
     main()
