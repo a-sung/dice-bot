580af4ad59b73afca4971faa9e46bd4d5a0d444e
Asung <a.city521@gmail.com>
2022-06-26 15:12:42 +0900

    fix: google api key file을 환경변수로 치환

--- src/auth/google.py
+++ src/auth/google.py
@@ -1,21 +1,27 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
+import os
 
 class GoogleAuth():
     def __init__(self):
         self.scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        self.keyfile = "../../t-bots-06202329-0a7625fa5fa6.json"
         self.filename = "coc-dice-bot"
 
+        self.variables_keys = {
+            "type": os.environ.get("SHEET_TYPE"),
+            "project_id": os.environ.get("SHEET_PROJECT_ID"),
+            "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
+            "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+            "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
+            "client_id": os.environ.get("SHEET_CLIENT_ID"),
+            "auth_uri": os.environ.get("SHEET_AUTH_URI"),
+            "token_uri": os.environ.get("SHEET_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.environ.get("SHEET_CLIENT_X509_CERT_URL")
+        }
+
+
     def get_auth(self):
-        credit = ServiceAccountCredentials.from_json_keyfile_name(self.keyfile, self.scope)
-        client = gspread.authorize(credit)
-        gc = client.open(self.filename)
+        credit = gspread.service_account_from_dict(self.variables_keys)
+        gc = credit.open(self.filename)
         return gc
-
-
-# # 시트 탭 선택 함수
-# def select_sheet(sheet_name):
-#     worksheet = wks.get_worksheet(0) # 시트 인덱스로 접근
-#     # worksheet = wks.worksheet(sheet_name) # 시트 이름으로 접근
-#     return worksheet
